c2b7041263ff0364ee6332db8f7bc645b1fbb3f6
k-amaya <giankevin9809@gmail.com>
2024-05-30 14:34:22 -0500

    Project start

---
+++ src/main/java/com/kamaya/code_test/CodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.kamaya.code_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeTestApplication.class, args);
+	}
+
+}
